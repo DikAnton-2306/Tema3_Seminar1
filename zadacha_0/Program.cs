1be76682a0f1038ae0664bf4569b422baa51574e
Антон Дик <antondik@yandex.ru>
2022-12-07 18:49:17 +0300

    Задача 3. Написать программу, которая будет выдавать название дня недели по заданному номеру

--- zadacha_0/Program.cs
+++ zadacha_0/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат
 Console.Clear(); //Clear() очищает терминал
 Console.Write("Введите любое число: ");
-int number = int.Parse(Console.ReadLine());//Parse() преобразовывает/конвертирует один тип данных в другой
+int number = int.Parse(Console.ReadLine()); //Parse() преобразовывает/конвертирует один тип данных в другой
 int square = number * number;
 Console.WriteLine($"Квадрат числа {number} равен {square}");//$ обозначает, что есть {переменные}
---
+++ zadacha_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Задача 3. Написать программу, которая будет выдавать название дня недели по заданному номеру
+Console.Clear(); //Clear() очищает терминал
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1) Console.WriteLine ("Это - понедельник");
+if (number == 2) Console.WriteLine ("Это - вторник");
+if (number == 3) Console.WriteLine ("Это - среда");
+if (number == 4) Console.WriteLine ("Это - четверг");
+if (number == 5) Console.WriteLine ("Это - пятница");
+if (number == 6) Console.WriteLine ("Это - суббота");
+if (number == 7) Console.WriteLine ("Это - воскресенье");
